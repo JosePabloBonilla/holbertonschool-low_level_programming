b366b514219bf849725b92842c5bb6cff840f039
JosePabloBonilla <2750@holbertonschool.com>
2021-02-28 22:25:29 +0000

    adding README.md holberton.h and 0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argc: number of arguments
+ * @argv: array containing number of arguments
+ * Return: 0 for success.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
