f0c8c76764486747ce7cc65e2aab1b61ec8a29be
JosePabloBonilla <2750@holbertonschool.com>
2021-03-04 22:11:13 +0000

    fixing file 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,37 +24,43 @@ int _strlen(char *s)
  * string_nconcat - concatenates two strings
  * @s1: string one
  * @s2: string two
+ * @n: integer that determines if s2 will be used
  * Description - concatenate two strings
  * Return: pointer to new space containing the 2 strings, if not NULL
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, l;
+	unsigned int l1, l2, l3;
 	char *new;
 
 	if (s1 == NULL)
 		s1 = "";
 
-	i = _strlen(s1);
+	l1 = _strlen(s1);
 
 	if (s2 == NULL)
 		s2 = "";
 
-	l = _strlen(s2);
-	l = n;
+	l2 = _strlen(s2);
 
-	new = malloc((sizeof(char) * (i + l) + 1));
+	if (n >= l2)
+		l3 = l1 + l2 + 1;
+
+	else
+		l2 = n;
+
+	new = malloc(l3);
 
 	if (new == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
-		new[i] = s1[i];
+	for (l1 = 0; s1[l1] != '\0'; l1++)
+		new[l1] = s1[l1];
 
-	for (l = 0; s2[l] != '\0'; l++)
-		new[i + l] = s2[l];
+	for (l2 = 0; s2[l2] != '\0'; l2++)
+		new[l1 + l2] = s2[l2];
 
-	new[i + l] = s2[l];
+	new[l1 + l2] = s2[l2];
 	return (new);
 }
