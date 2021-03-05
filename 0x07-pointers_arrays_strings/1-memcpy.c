65c7f3e02e08ed74a0e42911f784c3c3ae694e98
JosePabloBonilla <2750@holbertonschool.com>
2021-03-05 15:04:53 +0000

    better code on file 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,9 +10,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	for (; n > 0; n--)
+	unsigned int i;
+
+	for(i = 0; i < n; i++)
 	{
-		dest[n - 1] = src[n - 1];
+		dest[i] = src[i];
 	}
 
 	return (dest);
