a548478639d6c90e07832c9f1eaab2bf20b33cb9
JosePabloBonilla <2750@holbertonschool.com>
2021-03-09 00:02:05 +0000

    adding if to file 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initialize a varible of type struct dog
@@ -6,10 +7,14 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ * Return: nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
