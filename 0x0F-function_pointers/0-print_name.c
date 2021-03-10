4e56a7bf4232439ea7cae08e843675344b446513
JosePabloBonilla <2750@holbertonschool.com>
2021-03-10 20:01:55 +0000

    adding README.md headerfile and file 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: name to print
+ * @f: function pointer
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+
+	f(name);
+}
