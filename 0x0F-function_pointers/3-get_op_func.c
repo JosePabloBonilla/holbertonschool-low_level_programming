026b2c8341f660e08fd29e8fe8391568b1a132b2
JosePabloBonilla <2750@holbertonschool.com>
2021-03-11 02:14:32 +0000

    adding files for 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function
+ * @s: operator
+ * Return: pointer to function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of integers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of integers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product
+ * @a: first integer
+ * @b: second integer
+ * Return: product of integers
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: first integer
+ * @b: second integer
+ * Return: division of integers
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of division
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
