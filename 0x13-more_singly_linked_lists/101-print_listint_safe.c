5b8369ad4b6c7f29a5896918a86e28ce91252b98
JosePabloBonilla <2750@holbertonschool.com>
2021-03-24 03:37:34 +0000

    fix file 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,25 +7,28 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tmp1, *tmp2;
+	const listint_t *tmp;
+	int node;
 	size_t num;
 
-	tmp1 = head;
-	if (tmp1 == NULL)
+	tmp = head;
+	if (tmp == NULL)
 	{
 		return (0);
 		exit(98);
 	}
 	num = 0;
-	while (tmp1 != NULL)
+	while (tmp != NULL)
 	{
-		tmp2 = tmp1;
-		tmp1 = tmp1->next;
+		node = tmp - tmp->next;
 		num++;
-		printf("[%p] %d\n", (void *)tmp2, tmp2->n);
-		if (tmp2 <= tmp1)
+		printf("[%p] %d\n", (void *)tmp, tmp->n);
+		if (node > 0)
+			tmp = tmp->next;
+
+		else
 		{
-			printf("-> [%p] %d\n", (void *)tmp1, tmp1->n);
+			printf("-> [%p] %d\n", (void *)tmp->next, tmp->next->n);
 			break;
 		}
 	}
