7950e8878ff1550afe3697101671e9c7cf9b49a5
JosePabloBonilla <2750@holbertonschool.com>
2021-03-24 00:34:15 +0000

    fix file 10 edge case

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *tmp, *prev;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (-1);
 
 	tmp = *head;
