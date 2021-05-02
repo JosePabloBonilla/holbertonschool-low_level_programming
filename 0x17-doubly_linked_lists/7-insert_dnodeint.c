222b52b4f776e9e6505ce694859106c6f1e85f29
JosePabloBonilla <2750@holbertonschool.com>
2021-05-02 15:35:49 +0000

    fix file 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,21 @@
 #include "lists.h"
 /**
+ * dlistint_len - print all nodes of list
+ * @h: head of list
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
+/**
  * insert_dnodeint_at_index - insert new node
  * @h: head of list
  * @idx: index (position)
@@ -9,33 +25,41 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *current;
+	size_t length;
 	unsigned int i = 0;
-	
-	new = *h;
-	current = NULL;
+
 	if (h == NULL)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	new = malloc(sizeof(dlistint_t));
-	if (current == NULL)
-		return (NULL);
-	current->n = n;
+	length = dlistint_len(*h);
+	if (idx == length - 1)
+		return (add_dnodeint_end(h, n));
 
-	while (new)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*h == NULL)
+	{
+		new->prev = NULL;
+		new->next = NULL;
+		*h = new;
+		return (new);
+	}
+	current = *h;
+	while (current)
 	{
 		if (i == idx)
 		{
-			current->next = new->prev;
-			current->next = new;
-			new->prev = current;
-			current->prev->next = current;
-			return (current);
+			new->next = current;
+			new->prev = current->prev;
+			current->prev->next = new;
+			current->prev = new;
+			return (new);
 		}
-		if (new->next == NULL && idx == i + 1)
-			return (free(current), add_dnodeint_end(h, n));
-		new = new->next;
+		current = current->next;
 		i++;
 	}
 	free(current);
